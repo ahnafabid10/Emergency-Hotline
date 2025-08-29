9f739b6a6ea60f5b44d1751997b14fdc0442cfe7
Ahnaf Abid <ahnafabid600@gmail.com>
2025-08-29 16:47:32 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,101 @@
+// herHeart
+document.getElementById('her-heart')
+.addEventListener('click', function(){
+    const herHeart = document.getElementById('count-her-heart')
+    console.log(herHeart)
+    const countHerHeart = parseInt(herHeart.innerText)
+    console.log(countHerHeart)
+    const totalCount = countHerHeart + 1;
+    console.log(totalCount)
+    document.getElementById('count-her-heart').innerText = totalCount
+})
+
+// police heart 
+document.getElementById('police-heart')
+.addEventListener('click', function(){
+    const herHeart = document.getElementById('count-her-heart')
+    console.log(herHeart)
+    const countHerHeart = parseInt(herHeart.innerText)
+    console.log(countHerHeart)
+    const totalCount = countHerHeart + 1;
+    console.log(totalCount)
+    document.getElementById('count-her-heart').innerText = totalCount
+})
+// fire heart
+document.getElementById('fire-heart')
+.addEventListener('click', function(){
+    const herHeart = document.getElementById('count-her-heart')
+    console.log(herHeart)
+    const countHerHeart = parseInt(herHeart.innerText)
+    console.log(countHerHeart)
+    const totalCount = countHerHeart + 1;
+    console.log(totalCount)
+    document.getElementById('count-her-heart').innerText = totalCount
+})
+// ambulance-heart
+document.getElementById('ambulance-heart')
+.addEventListener('click', function(){
+    const herHeart = document.getElementById('count-her-heart')
+    console.log(herHeart)
+    const countHerHeart = parseInt(herHeart.innerText)
+    console.log(countHerHeart)
+    const totalCount = countHerHeart + 1;
+    console.log(totalCount)
+    document.getElementById('count-her-heart').innerText = totalCount
+})
+// Women-Child
+document.getElementById('Women-Child-heart')
+.addEventListener('click', function(){
+    const herHeart = document.getElementById('count-her-heart')
+    console.log(herHeart)
+    const countHerHeart = parseInt(herHeart.innerText)
+    console.log(countHerHeart)
+    const totalCount = countHerHeart + 1;
+    console.log(totalCount)
+    document.getElementById('count-her-heart').innerText = totalCount
+})
+//Anti-Corruption
+document.getElementById('Anti-Corruption-heart')
+.addEventListener('click', function(){
+    const herHeart = document.getElementById('count-her-heart')
+    console.log(herHeart)
+    const countHerHeart = parseInt(herHeart.innerText)
+    console.log(countHerHeart)
+    const totalCount = countHerHeart + 1;
+    console.log(totalCount)
+    document.getElementById('count-her-heart').innerText = totalCount
+})
+// Electricity
+document.getElementById('Electricity-heart')
+.addEventListener('click', function(){
+    const herHeart = document.getElementById('count-her-heart')
+    console.log(herHeart)
+    const countHerHeart = parseInt(herHeart.innerText)
+    console.log(countHerHeart)
+    const totalCount = countHerHeart + 1;
+    console.log(totalCount)
+    document.getElementById('count-her-heart').innerText = totalCount
+})
+//Brac Heart
+document.getElementById('Brac-heart')
+.addEventListener('click', function(){
+    const herHeart = document.getElementById('count-her-heart')
+    console.log(herHeart)
+    const countHerHeart = parseInt(herHeart.innerText)
+    console.log(countHerHeart)
+    const totalCount = countHerHeart + 1;
+    console.log(totalCount)
+    document.getElementById('count-her-heart').innerText = totalCount
+})
+//Bangladesh Railway
+document.getElementById('Bangladesh-Railway-heart')
+.addEventListener('click', function(){
+    const herHeart = document.getElementById('count-her-heart')
+    console.log(herHeart)
+    const countHerHeart = parseInt(herHeart.innerText)
+    console.log(countHerHeart)
+    const totalCount = countHerHeart + 1;
+    console.log(totalCount)
+    document.getElementById('count-her-heart').innerText = totalCount
+})
+
